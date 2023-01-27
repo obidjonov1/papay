d4efd76f4b4d63a249f59c7496c73a226775266d
obidjonov1 <obidjonov@naver.com>
2023-01-28 05:28:55 +0900

    feat: bcrypt package asosida passwordni hash qilish

--- models/Member.js
+++ models/Member.js
@@ -1,16 +1,21 @@
 const MemberModel = require("../schema/member.model");
 const Definer = require("../lib/mistake");
 const assert = require("assert");
+const bcrypt = require("bcryptjs"); // passwordni shifrlash uchun
 
 class Member {
   constructor() {
-    // shartli ravishda kichkina harfda yozib oldik
+    // shartli ravishda kichkina harfda yozib oldik.
     this.memberModel = MemberModel;
   }
   // req.body kelyapti
   async signupData(input) {
     try {
+      // passwordni shifrlash uchun ->
+      const salt = await bcrypt.genSalt();
+      input.mb_password = await bcrypt.hash(input.mb_password, salt);
       const new_member = new this.memberModel(input);
+
       let result;
       try {
         result = await new_member.save();
@@ -33,8 +38,12 @@ class Member {
         .exec();
       // agar member nick true bo'lsa ok, bo'lmasa Definer(ERROR) ->
       assert.ok(member, Definer.auth_err2);
-
-      const isMatch = input.mb_password === member.mb_password;
+      
+      // bu yerda login bo'layotganda bcrypt inputdagi pswni databaseagi psw bilan o'zi solishtirib oladi. 
+      const isMatch = await bcrypt.compare(
+        input.mb_password,
+        member.mb_password
+      );
       // agar member password true bo'lsa ok, bo'lmasa Definer(ERROR) ->
       assert.ok(isMatch, Definer.auth_err3);
 
