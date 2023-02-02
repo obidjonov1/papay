7f6119265afc01ba2ef2ef418cfe484b15c9fcf3
obidjonov1 <obidjonov@naver.com>
2023-02-02 18:55:42 +0900

    fix: modify image uploader utils

--- utils/upload-multer.js
+++ utils/upload-multer.js
@@ -1,9 +1,33 @@
+//Serverga image yuklash
 const path = require("path");
 const multer = require("multer");
 const uuid = require("uuid");
 const { randomBytes } = require("crypto");
 
-const product_storage = multer.diskStorage({
+/** MULTER IMAGE UPLOADER */
+function getTargetImageStorage(address) {
+  return multer.diskStorage({
+    destination: function (req, file, cb) {
+      cb(null, `./uploads/${address}`);
+    },
+    filename: function (req, file, cb) {
+      console.log(file);
+      // yuklanayotgan rasimni asl nomi va o'sha file farmatini olish uchun ->
+      const extension = path.parse(file.originalname).ext;
+      const random_name = uuid.v4() + extension;
+      cb(null, random_name);
+    },
+  });
+}
+
+const makeUploader = (address) => {
+  const storage = getTargetImageStorage(address);
+  return multer({ storage: storage });
+};
+
+module.exports = makeUploader;
+
+/* const product_storage = multer.diskStorage({
   destination: function (req, file, cb) {
     cb(null, "./uploads/products");
   },
@@ -16,4 +40,4 @@ const product_storage = multer.diskStorage({
   },
 });
 
-module.exports.uploadProductImage = multer({ storage: product_storage });
+module.exports.uploadProductImage = multer({ storage: product_storage }); */
