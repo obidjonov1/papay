fc8c34855d6b10a94636992eff8e5f69eb0fdcf2
obidjonov1 <obidjonov@naver.com>
2023-02-04 19:41:42 +0900

    feat: yangi productlarni restaurant userlar hosil qilish

--- controllers/productController.js
+++ controllers/productController.js
@@ -1,3 +1,7 @@
+const assert = require("assert");
+const Definer = require("../lib/mistake");
+const Product = require("../models/Product");
+
 let productController = module.exports;
 
 productController.getAllproducts = async (req, res) => {
@@ -12,8 +16,24 @@ productController.getAllproducts = async (req, res) => {
 productController.addNewProduct = async (req, res) => {
   try {
     console.log("POST: cont/addNewProduct");
-    // Product creation develop
-    res.send("ok");
+    // fileni yuklaydi agar file yo'q bo'lsa error qaytaradi ->
+    assert(req.files, Definer.general_err3);
+
+    const product = new Product();
+    let data = req.body;
+
+    // req.filesga yuklangan fileni array qilib databasega yozish ->
+    data.product_images = req.files.map((ele) => {
+      return ele.path;
+    });
+
+    const result = await product.addNewProductData(data, req.member);
+
+    const html = `<script>
+                    alert(new dush added successfully);
+                    window.location.replace('/resto/products/menu');
+                  </script>`;
+    res.end(html);
   } catch (err) {
     console.log(`ERROR: cont/addNewProduct ${err.message}`);
   }
