98641f7b90cd930935e3f96a1e16ebadcf9c79b1
obidjonov1 <obidjonov@naver.com>
2023-02-04 20:13:08 +0900

    feat: develop restaurant users editing their products

--- controllers/productController.js
+++ controllers/productController.js
@@ -42,6 +42,15 @@ productController.addNewProduct = async (req, res) => {
 productController.updateChosenProduct = async (req, res) => {
   try {
     console.log("POST: cont/updateChosenProduct");
+    const product = new Product();
+    const id = req.params.id;
+    const result = await product.updateChosenProductData(
+      id,
+      req.body,
+      req.member._id
+    );
+    // hammasi aytilganidek(true) bo'lsa javobni qaytarsin ->
+    await res.json({ state: "sucess", data: result });
   } catch (err) {
     console.log(`ERROR: cont/updateChosenProduct ${err.message}`);
   }
