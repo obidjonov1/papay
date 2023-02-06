015806221a43a9eb4b4bf317b22f4f1f22c21efd
obidjonov1 <obidjonov@naver.com>
2023-02-06 16:59:06 +0900

    feat: develop adminka homepage

--- controllers/restaurantController.js
+++ controllers/restaurantController.js
@@ -3,14 +3,24 @@ const Product = require("../models/Product");
 
 let restaurantController = module.exports;
 
+restaurantController.home = (req, res) => {
+  try {
+    console.log("GET: cont/home");
+    res.render("home-page");
+  } catch (err) {
+    console.log(`ERROR: cont/home, ${err.message}`);
+    res.json({ state: "fail", message: err.message });
+  }
+};
+
 restaurantController.getMyRestaurantProducts = async (req, res) => {
   try {
-    console.log("GET: cont/getMyRestaurantData");
+    console.log("GET: cont/getMyRestaurantProducts");
     const product = new Product();
     const data = await product.getAllProductsDataResto(res.locals.member);
     res.render("restaurant-menu", { restaurant_data: data });
   } catch (err) {
-    console.log(`ERROR: cont/getMyRestaurantData ${err.message}`);
+    console.log(`ERROR: cont/getMyRestaurantProducts ${err.message}`);
     res.json({ state: "fail", message: err.message });
   }
 };
